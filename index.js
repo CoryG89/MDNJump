fd0621afd56613866a1e931479b8c727dd004287
ishu3101 <ishu3101@hotmail.com>
2016-02-14 18:26:06 +1300

    Refactor code for opening url to remove duplicates

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function getLucky (query, site) {
     return [googleUrl, site, query].join('');
 }
 
+/** Opens a url with the query as the search term in the new tab*/
+function open_url(query){
+    var luckyUrl = getLucky(query, mdnUrl);
+    chrome.tabs.create({ url: luckyUrl });
+}
+
 /** Creates a new item in the context menu to open the top search result on a
     given site for the text selection on the page */
 chrome.contextMenus.create({
@@ -22,8 +28,7 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "mdnjump") {
         var query = info.selectionText;
-        var luckyUrl = getLucky(query, mdnUrl);
-        chrome.tabs.create({ url: luckyUrl });
+        open_url(query);
     }
 });
 
@@ -32,6 +37,5 @@ chrome.omnibox.setDefaultSuggestion({
     description: 'Press ENTER to jump to the top MDN page for: %s'
 });
 chrome.omnibox.onInputEntered.addListener(function (query) {
-    var luckyUrl = getLucky(query, mdnUrl);
-    chrome.tabs.update({ url: luckyUrl });
+    open_url(query);
 });
